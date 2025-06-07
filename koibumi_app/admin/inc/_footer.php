b8b901a4f6f122894c5e267ec8840657eccaf1c5
puwachin <ovs26228725169@icloud.com>
2025-06-07 23:57:07 +0900

    Delete koibumi_app directory

--- koibumi_app/admin/inc/_footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-  <p><a href="https://do.gt-gt.org/" target="_blank">コイブミ（Ver<?php echo $koibumiVersion; ?>）</a></p>
-</footer>
-
-</div>
-
-<script src="func.js"></script>
-</body>
-</html>
